b854f94a15e221e804dc35f6c27a1927bba7c101
Anand Munjuluri <100225249+Anand-Munjuluri@users.noreply.github.com>
2024-01-03 21:28:31 +0530

    Update index.js

--- week-2/class-codes/index.js
+++ week-2/class-codes/index.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const fs = require('fs');
+fs.readFile("a.txt", "utf8", function(err, data)
+{
+
+})
 
 app.use(bodyParser.json());
-app.post('/', (req, res) => {
+app.post('/', function(req, res){
     console.log(req.body);
     res.send('<h1>Hello World!</h1>');
 }
